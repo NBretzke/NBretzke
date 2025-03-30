7eabd64c625bfd1504dd1ceb2097ea25f35c030f
Nicholas Bretzke <nbretzk@gmail.com>
2025-03-30 16:27:47 -0500

    Update WN10-CC-000326.ps1

--- STIGS/WN10-CC-000326.ps1
+++ STIGS/WN10-CC-000326.ps1
@@ -1,1 +1,33 @@
+ Author          : Nicholas Bretzke
+    LinkedIn        : linkedin.com/in/nicholas-bretzke-931735164/
+    GitHub          : github.com/NBretzke
+    Date Created    : 2025-30-03
+    Last Modified   : 2025-30-03
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-AU-000500
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-CC-000326).ps1 
+#>
+# Enable PowerShell Script Block Logging
+$regPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging"
+
+# Create the registry path if it does not exist
+if (-Not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force
+}
+
+# Set the registry value to enable script block logging
+Set-ItemProperty -Path $regPath -Name "EnableScriptBlockLogging" -Value 1 -Type DWord
+
+Write-Output "PowerShell Script Block Logging has been enabled."
 
