f8383cd0295b3bbcd6c6d73854811342f36c2d77
Nicholas Bretzke <nbretzk@gmail.com>
2025-03-29 21:47:47 -0500

    Update WN10-SO-000030.ps1

--- STIGS/WN10-SO-000030.ps1
+++ STIGS/WN10-SO-000030.ps1
@@ -1,4 +1,23 @@
-# Check if audit policy subcategories are enabled
+    Author          : Nicholas Bretzke
+    LinkedIn        : linkedin.com/in/nicholas-bretzke-931735164/
+    GitHub          : github.com/NBretzke
+    Date Created    : 2025-29-03
+    Last Modified   : 2024-29-03
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-AU-000500
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-SO-000030).ps1 # Check if audit policy subcategories are enabled
+    
 $auditStatus = Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" -Name "SCENoApplyLegacyAuditPolicy" -ErrorAction SilentlyContinue
 
 if ($auditStatus.SCENoApplyLegacyAuditPolicy -ne 1) {
